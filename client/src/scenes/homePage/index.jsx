e22f891b60ff88f2769623cbf301f3c5f1c9c720
waleedsaifullah <waleedsaifullah786@gmail.com>
2022-12-24 15:36:25 -0800

    [ADD] Post functionality on home page

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,7 +1,9 @@
 import { Box, useMediaQuery } from '@mui/material';
 import { useSelector } from 'react-redux';
 import Navbar from 'scenes/navbar';
+import MyPostWidget from 'scenes/widgets/MyPostWidget';
 import UserWidget from 'scenes/widgets/UserWidget';
+ 
 
 const HomePage = () => {
     const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
@@ -27,6 +29,7 @@ const HomePage = () => {
                     flexBasis={isNonMobileScreens ? "42%" : undefined}
                     mt={isNonMobileScreens ? undefined : "2rem"}
                 >
+                    <MyPostWidget picturePath={picturePath}/>
                 </Box>
                 {isNonMobileScreens && <Box flexBasis="26%"></Box>}
             </Box>
